171a702bbbbb1bcdd4a6981d824af76e8d51c81c
mvreddy30 <iam.venkatreddie@gmail.com>
2020-08-30 19:13:04 -0400

    tarun

---
+++ project3.js
@@ -0,0 +1,39 @@
+var express=require('express');
+const app =express();
+
+var mysql=require('mysql');
+
+
+var connection =mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:'userdata'
+});
+
+connection.connect(function(err){
+
+    if (err) throw err;
+    console.log("connected");
+
+    var itemName="adidas ";
+    var price="$100cad";
+   
+    var insertCmd="INSERT INTO order_info(itemName,price) values(?,?)";
+    var values=[itemName,price]
+    connection.query(insertCmd,values,function(err,result){
+     if(err) throw err;
+     console.log("1 entry recorded");
+
+
+
+
+    })
+});
+
+
+var server=app.listen(3019,()=>{
+    console.log("server running at http://localhost:3019");
+
+
+});
